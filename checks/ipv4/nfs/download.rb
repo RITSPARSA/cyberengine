ed3c9afc6de9d82da7b47b233a6d46eae85101a1
Jaroslaw Gorny <jgorny@trustwave.com>
2016-02-19 15:54:42 +0100

    Checks: IPv4: NFS: draft

---
+++ checks/ipv4/nfs/download.rb
@@ -0,0 +1,28 @@
+require 'scoring_engine'
+
+module ScoringEngine
+  module Checks
+
+    class NFSDownload < ScoringEngine::Engine::BaseCheck
+
+      FRIENDLY_NAME = "NFS Download"
+      PROTOCOL = "nfs"
+      VERSION = "ipv4"
+
+      def command_str
+        share = get_random_property('share')
+        raise("Missing share property") unless share
+        filename = get_random_property('filename')
+        raise("Missing filename property") unless filename
+        
+        cmd = "tmpdir=$(mktemp -d /tmp/nfs_mount.XXXX) && tmpfile=$(mktemp /tmp/nfs_file.XXXX) "
+        cmd << "&& mount -t nfs #{ip}:#{share} $tmpdir "
+        cmd << "&& cp $tmpdir/#{filename} $tmpfile "
+        cmd << "&& umount $tmpdir "
+
+        return cmd
+      end
+
+    end
+  end
+end
