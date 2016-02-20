50e4dc53aa090b36fdecd9b96d12ed73952e2f37
Brandon Myers <bdm7789@gmail.com>
2016-02-19 18:14:24 -0600

    Add warning if server is changed on service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -5,9 +5,9 @@ class ServicesController < ApplicationController
 
   before_filter :read_path
   def read_path
-    @team = Team.find_by_id(params[:team_id]) 
+    @team = Team.find_by_id(params[:team_id])
     redirect_to teams_path, alert: "Team #{params[:team_id]} does not exist" if @team.nil?
-    @server = Server.find_by_id(params[:server_id]) 
+    @server = Server.find_by_id(params[:server_id])
     redirect_to team_path(@team), alert: "Server #{params[:server_id]} does not exist" if @server.nil?
   end
 
@@ -39,8 +39,14 @@ class ServicesController < ApplicationController
   def update
     @service = Service.find_by_id(params[:id])
     authorized?(@service)
+    old_server = @service.server
     if @service.update_attributes(params[:service])
-      redirect_to team_path(@team), notice: 'Service was successfully updated'
+      if old_server.id.to_s != params[:service]["server_id"].to_s
+        new_server = Server.find_by_id(params[:service]["server_id"])
+        redirect_to team_path(@team), :flash => { :warning => "You have just changed the server for #{@service['name']} from #{old_server.name} to #{new_server.name}. You should verify this was intentional." }, notice: 'Service was successfully updated'
+      else
+        redirect_to team_path(@team), notice: 'Service was successfully updated'
+      end
     else
       render action: "edit"
     end
