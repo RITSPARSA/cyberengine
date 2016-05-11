20122699e22b41d78a86101115667f1d85773f1e
Ben Lipton <ben.lipton@gmail.com>
2016-05-11 13:37:13 -0400

    Shell escape SSH username and password
    Plus, use {} for quoting in expect string

--- checks/ipv4/ssh/login.rb
+++ checks/ipv4/ssh/login.rb
@@ -1,4 +1,5 @@
 require 'scoring_engine'
+require 'shellwords'
 
 module ScoringEngine
   module Checks
@@ -9,16 +10,20 @@ module ScoringEngine
       PROTOCOL = "ssh"
       VERSION = "ipv4"
 
+      def escape(str)
+        return str.gsub('}','\\}').gsub('{', '\\{').shellescape
+      end
+
       def command_str
         user = service.users.sample
         raise "Missing users" unless user
-        username = user.username
-        password = user.password
+        username = escape(user.username)
+        password = escape(user.password)
 
         command = get_random_property("command")
         raise "Missing command" unless command
 
-        cmd = "expect -c 'spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no #{username}@#{ip} #{command}; expect \"assword\"; send \"#{password}\r\"; interact'"
+        cmd = "expect -c 'spawn ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no {'#{username}'@#{ip}} #{command}; expect \"assword\"; send {'#{password}'\r}; interact'"
 
         return cmd
       end
